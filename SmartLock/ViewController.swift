b2b6b1c47d3c6b11bc167f7d5a4af35e69f2316f
Mason Woodford <63433735+masonwoodford@users.noreply.github.com>
2022-02-05 19:39:15 -0800

    initial commit

---
+++ SmartLock/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SmartLock
+//
+//  Created by Mason Woodford on 2/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
