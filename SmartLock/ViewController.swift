bfb36414aff319c05efcf4893f3727d838377867
Mason Woodford <63433735+masonwoodford@users.noreply.github.com>
2022-02-05 21:52:39 -0800

    basic homepage

--- SmartLock/ViewController.swift
+++ SmartLock/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var unlockButton: UIButton!
+    @IBOutlet weak var lockControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        unlockButton.tintColor = UIColor.red
+        unlockButton.setTitle("Unlock", for: UIControl.State.normal)
     }
-
+    
+    @IBAction func toggleLock(_ sender: Any) {
+        if unlockButton.tintColor == UIColor.green {
+            unlockButton.tintColor = UIColor.red
+        } else {
+            unlockButton.tintColor = UIColor.green
+        }
+        if unlockButton.title(for: UIControl.State.normal) == "Unlock" {
+            unlockButton.setTitle("Lock", for: UIControl.State.normal)
+        } else {
+            unlockButton.setTitle("Unlock", for: UIControl.State.normal)
+        }
+    }
+    
 
 }
 
